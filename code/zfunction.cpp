f7c3595aee9dab33ed93b143f3f89b6fc242572a
Victor Colombo <victorcolombo@gmail.com>
2018-11-08 01:33:37 -0200

    Removing old stuff

--- code/zfunction.cpp
+++
@@ -1,7 +0,0 @@
-void Z(char s[], int n, int z[]) { // z[i] = |lcp(s,s[i..n])|
-	for(int i = 1, m = -1; i < n; i++) {
-		z[i] = (m != -1 && m + z[m] >= i)?min(m + z[m] - i, z[i - m]):0;
-		while (i + z[i] < n && s[i + z[i]] == s[z[i]]) z[i]++;
-		if (m == -1 || i + z[i] > m + z[m]) m = i;
-	}
-}
